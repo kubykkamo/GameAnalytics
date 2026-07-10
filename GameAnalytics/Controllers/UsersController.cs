3a6a88bf3ee1ea926b7991121011f02f86a0626d
kubykkamo <gamingstudio39@gmail.com>
2026-07-10 14:14:23 +0200

    Initial setup: ASP.NET Core API with EF Core, SQLite and UsersController

---
+++ GameAnalytics/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GameAnalytics.Data;
+using GameAnalytics.Models;
+using System.Net.Http;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace GameAnalytics.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+
+
+    public class UsersController : ControllerBase
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] User user)
+        {
+            
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok(user);
+        }
+        [HttpGet]
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+
+
+    }
+}
+ 
---
+++ GameAnalytics/Models/User.cs
@@ -0,0 +1,23 @@
+﻿namespace GameAnalytics.Models
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string DiscordId { get; set; }
+
+
+        public User(string username, string discordId = null)
+        {
+            
+            Username = username;
+            DiscordId = discordId;
+        }
+
+
+
+    }
+}
