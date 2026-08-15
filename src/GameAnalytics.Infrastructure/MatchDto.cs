48feed68dcd1e4858d6923f90aafe3056a3b0581
kubykkamo <gamingstudio39@gmail.com>
2026-08-15 20:48:21 +0200

    Update solution file and match DTOs

--- src/GameAnalytics.Infrastructure/MatchDto.cs
+++ src/GameAnalytics.Infrastructure/MatchDto.cs
@@ -4,32 +4,32 @@ namespace GameAnalytics.Infrastructure
 {
     public class MatchHistoryResponseDto
     {
-        public List<MatchDataDto> Data { get; set; }
+        public required List<MatchDataDto> Data { get; set; }
 
     }
 
     public class SingleMatchResponseDto
     {
-        public MatchDataDto Data { get; set; }
+        public required MatchDataDto Data { get; set; }
         }
 
     public class MatchDataDto
     {
-        public MatchMetaData Metadata { get; set; }
-        public List<PlayerDto> Players { get; set; }
+        public required MatchMetaData Metadata { get; set; }
+        public required List<PlayerDto> Players { get; set; }
 
-        public List<TeamDto> Teams { get; set; }
-        public List<RoundDto> Rounds { get; set; } 
+        public required List<TeamDto> Teams { get; set; }
+        public required List<RoundDto> Rounds { get; set; } 
     }
 
     public class TeamDto
     {
         [JsonPropertyName("team_id")]
-        public string TeamId { get; set; } 
+        public required string TeamId { get; set; } 
 
         public bool Won { get; set; }
 
-        public TeamRoundsDto Rounds { get; set; }
+        public required TeamRoundsDto Rounds { get; set; }
     }
 
     public class TeamRoundsDto
@@ -39,37 +39,37 @@ namespace GameAnalytics.Infrastructure
     }
     public class MatchMetaData
     {
-        public Map Map { get; set; }
+        public required Map Map { get; set; }
         [JsonPropertyName("started_at")]
-        public string StartedAt { get; set; }
+        public required string StartedAt { get; set; }
 
-        public Queue queue { get; set; }
+        public required Queue queue { get; set; }
 
         [JsonPropertyName("match_id")]
-        public string MatchId { get; set; }
+        public required string MatchId { get; set; }
     }
 
     public class Queue
     {
-        public string Name { get; set; }
+        public required string Name { get; set; }
     }
     
     public class Map
     {
-        public string Name { get; set; }
+        public required string Name { get; set; }
     } 
 
     public class AccountResponseDto 
     { 
-        public AccountData Data { get; set; }
+        public required AccountData Data { get; set; }
     }
 
     public class AccountData 
     { 
-        public string Puuid { get; set; }
+        public required string Puuid { get; set; }
         [JsonPropertyName("account_level")] 
         public int AccountLevel { get; set; }
-        public string Card { get; set; }
+        public required string Card { get; set; }
     
     }
 
--- src/GameAnalytics.Infrastructure/MatchListDto.cs
+++ src/GameAnalytics.Infrastructure/MatchListDto.cs
@@ -4,18 +4,18 @@ namespace GameAnalytics.Infrastructure
 {
     public class MatchListDto
     {
-        public List<MatchItemDto> Data{ get; set; }
+        public required List<MatchItemDto> Data{ get; set; }
     }
     public class MatchItemDto
     {
-        public MatchMetaDto MetaData {  get; set; }
+        public required MatchMetaDto MetaData {  get; set; }
 
     }
 
     public class MatchMetaDto
     {
         [JsonPropertyName("match_id")]
-        public string Id { get; set; }
+        public required string Id { get; set; }
     }
 
 
